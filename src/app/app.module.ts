e82d91540b9b670500c64733026244a3757f56a4
Dolan Miu <dolan_miu@hotmail.com>
2018-02-11 18:17:31 +0000

    Add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, NavBarComponent, FooterComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
